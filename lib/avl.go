72ee3727dd4d723c77a8231c1719d6fd01237a0e
Pasindu Pramodya <git@pasindupramodya.com>
2025-03-19 21:38:11 +0530

    fix2

--- lib/avl.go
+++ lib/avl.go
@@ -1,1 +1,41 @@
 package lib
+
+type AVL struct {
+	root *Node
+}
+
+func NewAVL() *AVL {
+	return &AVL{}
+}
+
+func (avl *AVL) Insert(key string, value string) {
+	avl.root = avl.insert(avl.root, key, value)
+}
+
+func (avl *AVL) insert(node *Node, key string, value string) *Node {
+	if node == nil {
+		return NewNode(key, value)
+	}
+	if key < node.key {
+		node.left = avl.insert(node.left, key, value)
+	} else if key > node.key {
+		node.right = avl.insert(node.right, key, value)
+	} else {
+		return node
+	}
+	node.height = 1 + max
+}
+
+func (avl *AVL) max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func (avl *AVL) height(node *Node) int {
+	if node == nil {
+		return 0
+	}
+	return node.height
+}
--- lib/node.go
+++ lib/node.go
@@ -1,1 +1,17 @@
 package lib
+
+type Node struct {
+	key    string
+	value  string
+	height int
+	left   *Node
+	right  *Node
+}
+
+func NewNode(key string, value string) *Node {
+	return &Node{
+		key:    key,
+		value:  value,
+		height: 1,
+	}
+}
